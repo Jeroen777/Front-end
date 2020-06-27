09a3c5498b5100809f8362dc573b856df0bd40de
Jeroen777 <jeroenbesselink98@gmail.com>
2020-06-28 00:09:25 +0200

    verbergen info met progressive enchancement

--- js/functie.js
+++ js/functie.js
@@ -1,22 +1,41 @@
-// const info = document.querySelector('#verstopInfo', 'verstopInfoTwee');
+//variablen aanmaken
+let info = document.querySelector('#alleMatches');
+let btn = document.querySelector('#knop');
 
-// function hideInfo() {
-//     info.classList.toggle("visible");
-//     console.log("testen");
-// };
+//functie om informatie van een match te verbergen
+function infoVerbergen() {
+    info.classList.toggle("visible");
+    console.log("testen");
+};
 
-// info.addEventListener("click", hideInfo);
-
-function scrollWeergave(){
-    var match = document.querySelector('#test');
+//Bron MDN. (2020a, 6 mei). Element.getBoundingClientRect(). Geraadpleegd van https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+//Functie om de matches in het scherm te laden faden zodra je op een bepaald gedeelte van het scherm scrollt
+function scrollWeergave() {
+    var match = document.querySelector('#alleMatches');
     var matchPlek = match.getBoundingClientRect().top;
     var schermPlek = window.innerHeight / 2;
 
-    if (matchPlek < schermPlek){
+    if (matchPlek < schermPlek) {
         match.classList.add('info-weergeven')
     }
     console.log(matchPlek);
 
 }
 
+//event listeners
+btn.addEventListener("click", infoVerbergen);
+
 window.addEventListener('scroll', scrollWeergave);
+
+//proberen met keydown event
+//checken welke knop er word gedrukt
+window.addEventListener('keydown', (event) => {
+    console.log(event);
+})
+
+//functie trigger met de A knop
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode == 65) {
+        info.classList.toggle("visible");
+    }
+});
